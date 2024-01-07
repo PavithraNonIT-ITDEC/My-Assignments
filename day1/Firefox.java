cb4a08423dd827e56db54c44b7ca6cb772db62fb
PavithraNonIT-ITDEC <155864004+PavithraNonIT-ITDEC@users.noreply.github.com>
2024-01-07 08:51:20 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+		
+		System.out.println("Browser Version :" + browserVersion);
+		System.out.println("Name of the Browser :" + browserName);
+		System.out.println("Year of Release :" + releaseYear);
+		System.out.println("Logo of the Browser :" +browserLogo);
+		System.out.println("Is Browser visible :" + isVisible);
+	}
+
+}
